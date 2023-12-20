b6c28a1c478c04f956420a4d8248154ddf7e1c96
Paul Kimball <pekimball@interfaceassociates.net>
2023-12-20 13:22:40 -0500

    Support path variable in request

--- src/main/java/com/pluralsight/springcrudrepo/ProductController.java
+++ src/main/java/com/pluralsight/springcrudrepo/ProductController.java
@@ -1,9 +1,7 @@
 package com.pluralsight.springcrudrepo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ProductController {
@@ -11,9 +9,14 @@ public class ProductController {
     @Autowired
     ProductRepository productDao;
 
-    @RequestMapping(method=RequestMethod.GET, path="/")
+    @RequestMapping(method=RequestMethod.GET, path="/anyproduct")
     public Product getProduct(){
         // Return product 42 just to prove it works
         return productDao.findById(42).get();
+    }
+
+    @RequestMapping(method=RequestMethod.GET, path="/products/{id}" )
+    public Product getProductById(@PathVariable int id){
+        return productDao.findById(id).get();
     }
 }
